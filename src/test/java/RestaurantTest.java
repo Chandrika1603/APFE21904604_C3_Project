4686301204ce15bd731a22ffec56369395787b06
Chandrika1603 <chndrkkv@gmail.com>
2021-08-28 13:16:47 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.time.LocalTime;
+import java.util.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
@@ -74,6 +75,42 @@ class RestaurantTest {
 
         assertThrows(itemNotFoundException.class,
                 ()->restaurant.removeFromMenu("French fries"));
+    }
+
+    @Test
+    public void calculateOrderCost_called_with_selected_items_from_the_menu_should_return_the_total_order_cost(){
+
+        //Arrange
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        restaurant.addToMenu("Dal soup",179);
+        restaurant.addToMenu("Vegetable curry", 369);
+
+        List<String> selectedItemNames= new ArrayList<String>();
+        selectedItemNames.add("Vegetable lasagne");
+        selectedItemNames.add("Dal soup");
+        int expectedCost = 269+179;
+        int totalCost = restaurant.calculateOrderCost(selectedItemNames);
+
+        //Assert
+        assertEquals(expectedCost,totalCost);
+    }
+
+    @Test
+    public void calculateOrderCost_called_with_0_items_from_the_menu_should_return_0_as_the_order_cost(){
+
+        //Arrange
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        restaurant.addToMenu("Dal soup",179);
+        restaurant.addToMenu("Vegetable curry", 369);
+
+        List<String> selectedItemNames= new ArrayList<String>();
+        int expectedCost = 0;
+        int totalCost = restaurant.calculateOrderCost(selectedItemNames);
+
+        //Assert
+        assertEquals(expectedCost,totalCost);
     }
 
     @BeforeEach
